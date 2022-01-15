34b942fb90764fcd9440e798f9dde053afffefe7
IvettSzoke <ivett.oze@gmail.com>
2022-01-15 11:26:46 +0100

    first three functions work

--- game_inventory.py
+++ game_inventory.py
@@ -1,25 +1,36 @@
-
-# This is the file where you must work.
-# Write code in the functions (and create new functions) so that they work
-# according to the requirements.
+import csv
 
 
 def display_inventory(inventory):
-    """Display the contents of the inventory in a simple way."""
-    pass
-
+    for item in inventory.items():
+        print(f"{item[0]}: {item[1]}")
+               
 
 def add_to_inventory(inventory, added_items):
+    for element in added_items:
+        if element in inventory:
+            inventory[element] += 1
+    return inventory
+   
     """Add to the inventory dictionary a list of items from added_items."""
-    pass
 
 
 def remove_from_inventory(inventory, removed_items):
     """Remove from the inventory dictionary a list of items from removed_items."""
-    pass
-
+    for element in removed_items:
+        if element in inventory:
+            if inventory[element] > 1:
+                inventory[element] -= 1
+            # if inventory[element] == 0
+            else:
+                inventory.pop(element)
+    return inventory
 
 def print_table(inventory, order=None):
+
+
+
+
     """
     Display the contents of the inventory in an ordered, well-organized table with
     each column right-aligned.
@@ -30,7 +41,12 @@ def print_table(inventory, order=None):
 
 def import_inventory(inventory, filename):
     """Import new inventory items from a CSV file."""
-
+     # with open('test_inventory.csv', 'r') as csv_file:
+    #     inventory = csv.reader(csv_file)
+    #     for line in inventory:
+    #         for item in line:
+    #             for key, value in item:
+    #                 print(key, ' : ', value)
     pass
 
 
