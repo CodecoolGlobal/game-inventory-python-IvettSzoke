41b3e15f3f320c286a74e98e7981c0a0f8acd2e1
IvettSzoke <ivett.oze@gmail.com>
2022-01-16 09:13:08 +0100

    csv file reading is OK, but stop by the iteration

--- game_inventory.py
+++ game_inventory.py
@@ -1,5 +1,5 @@
 import csv
-
+import pprint
 
 def display_inventory(inventory):
     for item in inventory.items():
@@ -10,9 +10,11 @@ def add_to_inventory(inventory, added_items):
     for element in added_items:
         if element in inventory:
             inventory[element] += 1
+        else:
+            inventory[element] = 1
     return inventory
-   
-    """Add to the inventory dictionary a list of items from added_items."""
+
+
 
 
 def remove_from_inventory(inventory, removed_items):
@@ -27,28 +29,61 @@ def remove_from_inventory(inventory, removed_items):
     return inventory
 
 def print_table(inventory, order=None):
+    inventory = {"rope": 1, "torch": 2}
+    my_list = inventory.items()
+
+    for key, value in my_list:
+        item_name, count = key, value
+        print ("{:<10} {:<10}".format('item_name', 'count'))
+
+        
+    # for row in zip(*([key] + value for key, value in sorted(inventory.items()))):
+    #     print(*row)
+# Insert data into dictionary
+# dict1 = {1: ["Samuel", 21, 'Data Structures'],
+#      2: ["Richie", 20, 'Machine Learning'],
+#      3: ["Lauren", 21, 'OOPS with java'],
+# #      }
+ 
+# # Print the names of the columns.
+#     # print ("{:<10} {:<10}".format('item_name', 'count'))
+#     print('item name', 'count')
+# # print each data item.
+#     # for key, value in inventory.items():
+#     #     item_name, count = value
+#     # print ("{:<10} {:<10}".format('item_name', 'count'))
+#     for i in range(2):
+#         for j in range(2):
+#             print(inventory[(i, j)])
+    # for each_row in zip(*([i] + (j)
+    #     for i, j in inventory.items())):
+    #         print(*each_row, " ")
+
+print_table(inventory={"rope": 1, "torch": 2})
 
 
 
 
-    """
-    Display the contents of the inventory in an ordered, well-organized table with
-    each column right-aligned.
-    """
-
-    pass
 
 
-def import_inventory(inventory, filename):
+
+def import_inventory(inventory, filename="test_inventory.csv"):
     """Import new inventory items from a CSV file."""
-     # with open('test_inventory.csv', 'r') as csv_file:
-    #     inventory = csv.reader(csv_file)
-    #     for line in inventory:
-    #         for item in line:
-    #             for key, value in item:
-    #                 print(key, ' : ', value)
-    pass
+    try:
+        with open(filename, mode = 'r') as csv_file:
+            input_csv = csv.reader(csv_file, delimiter=',')
+            for line in input_csv:
+                line.sort()
+                inventory = add_to_inventory(inventory, line)
+          
+        return inventory
 
+    except FileNotFoundError:
+        print(f"File '{filename}' not found!")
+
+
+# print(add_to_inventory({"rope": 1, "torch": 2}, ["rope", "rope"]))
+# print(import_inventory(inventory = {"rope": 1, "torch": 2}))
 
 def export_inventory(inventory, filename):
     """Export the inventory into a CSV file."""
